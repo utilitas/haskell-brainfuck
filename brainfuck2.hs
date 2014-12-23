51c9f0c6f1ee7cf9bd4ab627ba5af6b351a4634e
utilitas <utilitas.mater.iusti@gmail.com>
2014-12-23 15:45:09 +0900

    Update brainfuck2.hs

--- brainfuck2.hs
+++ brainfuck2.hs
@@ -1,7 +1,6 @@
-{-# LANGUAGE ViewPatterns #-}
 module Main where
+
 import Data.Foldable (foldlM)
-import Data.Sequence (Seq, empty, (|>), ViewR(..), viewr)
 import qualified Data.Vector.Unboxed.Mutable as M
 
 import System.Environment (getArgs)
@@ -22,33 +21,33 @@ data Instruction =  PIncr Int
                    |CDecr Word8
                    |Print
                    |Read
-                   |Loop (Seq Instruction)
+                   |Loop [Instruction]
                    deriving (Eq, Show)
 
 -- parse and compile a Brainfuck code to instructions.
-compile :: String -> Seq Instruction
-compile str = fst $ compile' (empty, str)
+compile :: String -> [Instruction]
+compile str = fst $ compile' ([], str)
   where
-    compile' :: (Seq Instruction, String) -> (Seq Instruction, String)
-    compile' (acc, "") = (acc, "")
-    compile' (viewr -> acc:>PIncr n, '>':xs) = compile' (acc |> PIncr (n+1), xs)
-    compile' (acc,'>':xs) = compile' (acc |> PIncr 1, xs)
-    compile' (viewr -> acc:>PDecr n, '<':xs) = compile' (acc |> PDecr (n+1), xs)
-    compile' (acc,'<':xs) = compile' (acc |> PDecr 1, xs)
-    compile' (viewr -> acc:>CIncr n, '+':xs) = compile' (acc |> CIncr (n+1), xs)
-    compile' (acc,'+':xs) = compile' (acc |> CIncr 1, xs)
-    compile' (viewr -> acc:>CDecr n, '-':xs) = compile' (acc |> CDecr (n+1), xs)
-    compile' (acc,'-':xs) = compile' (acc |> CDecr 1, xs)
-    compile' (acc,'.':xs) = compile' (acc |> Print, xs)
-    compile' (acc,',':xs) = compile' (acc |> Read, xs)
-    compile' (acc,'[':xs) = compile' (acc |> Loop subinstructs, xss)
-      where (subinstructs, xss) = compile' (empty, xs)
-    compile' (acc,']':xs) = (acc, xs)
+    compile' :: ([Instruction], String) -> ([Instruction], String)
+    compile' (acc, "") = (reverse acc, "")
+    compile' (PIncr n:acc, '>':xs) = compile' (PIncr (n+1):acc, xs)
+    compile' (PDecr n:acc, '<':xs) = compile' (PDecr (n+1):acc, xs)
+    compile' (CIncr n:acc, '+':xs) = compile' (CIncr (n+1):acc, xs)
+    compile' (CDecr n:acc, '-':xs) = compile' (CDecr (n+1):acc, xs)
+    compile' (acc,'>':xs) = compile' (PIncr 1:acc, xs)
+    compile' (acc,'<':xs) = compile' (PDecr 1:acc, xs)
+    compile' (acc,'+':xs) = compile' (CIncr 1:acc, xs)
+    compile' (acc,'-':xs) = compile' (CDecr 1:acc, xs)
+    compile' (acc,'.':xs) = compile' (Print:acc, xs)
+    compile' (acc,',':xs) = compile' (Read:acc, xs)
+    compile' (acc,'[':xs) = compile' (Loop subinstructs:acc, xss)
+      where (subinstructs, xss) = compile' ([], xs)
+    compile' (acc,']':xs) = (reverse acc, xs)
     compile' (acc, _:xs) = compile' (acc, xs) -- ignore any other character.
 
 -- execute an instruction on a machine with a given state.
 execute :: Machine -> Instruction -> IO Machine
-execute machine@(pointer, memory) instruct = case instruct of
+execute machine@(pointer, memory) instruct= case instruct of
   PIncr n -> return (pointer+n, memory)
   PDecr n -> return (pointer-n, memory)
   CIncr n -> do
@@ -70,13 +69,13 @@ execute machine@(pointer, memory) instruct = case instruct of
     where
       loop m@(ptr, mem) i = do
                               c <- M.read mem ptr
-                              if (i==empty) || c == 0
+                              if (i==[]) || c == 0
                                 then return m
                                 else do {m' <- run m i; loop m' i;}
 
 -- execute a sequence of instructions.
-run :: Machine -> Seq Instruction -> IO Machine
-run machine = foldlM execute machine
+run :: Machine -> [Instruction] -> IO Machine
+run = foldlM execute 
 
 -- read the first argument as a code, compile and execute it
 -- on a machine with an initial state.
